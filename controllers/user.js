24612737aac6c455fc944cdc15231201fb9d5daf
AgatheEdifixio <agathe.marcellini@edifixio.com>
2020-11-19 09:30:36 +0100

    envoi token coté front

--- controllers/user.js
+++ controllers/user.js
@@ -20,28 +20,26 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-    console.log("backend export login");
-      User.findOne({ email: req.body.email })
-        .then(user => {
-            if(!user){
-                return res.status(401).json({ error: 'utilisateur non trouvé !' })
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, user.password)
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    if(!valid){
-                        return res.status(401).json({ error })
-                    }
-                    res.status(200).json({
-                        userid: user._id,
-                        token: jwt.sign(
-                            { userId : user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
-                        )
-                    })
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-
-};
+            res.status(200).json({
+              userId: user._id,
+              token: jwt.sign(
+                { userId: user._id },
+                'RANDOM_TOKEN_SECRET',
+                { expiresIn: '24h' }
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
--- routes/plants.js
+++ routes/plants.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const plantCtrl = require('../controllers/plant');
 const auth = require('../middleware/auth');
 
- router.post('/', plantCtrl.createPlant); 
+/*  router.post('/', plantCtrl.createPlant); 
  router.get('/', plantCtrl.getAllPlants);
  router.get('/:id', plantCtrl.getOnePlant); 
  router.delete('/:id', plantCtrl.deletePlant);
- router.put('/:id', plantCtrl.modifyPlant); 
+ router.put('/:id', plantCtrl.modifyPlant); */
 
+  router.post('/', auth, plantCtrl.createPlant); 
+ router.get('/', auth, plantCtrl.getAllPlants);
+ router.get('/:id', auth, plantCtrl.getOnePlant); 
+ router.delete('/:id', auth, plantCtrl.deletePlant);
+ router.put('/:id', auth, plantCtrl.modifyPlant); 
+ 
 module.exports = router;
